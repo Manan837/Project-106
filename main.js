2ac4f454d090282c2b68e56b46abd48001eafb69
Manan837 <89606852+Manan837@users.noreply.github.com>
2023-01-15 12:01:24 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// https://teachablemachine.withgoogle.com/models/nLhcq9EdM/4
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassify('https://teachablemachine.withgoogle.com/models/nLhcq9EdM/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
